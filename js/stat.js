e5bbfdcd7ccd84704417c04cf32c70d52eeacbe4
IcyCrumb <ludysh21@gmail.com>
2017-01-24 00:02:35 +0300

    Вторая домашка3

--- js/stat.js
+++ js/stat.js
@@ -1,44 +1,4 @@
-'use strict'
-function renderStatistics(ctx, names, times) {
-
-ctx.shadowOffsetX = 10;
-ctx.shadowOffsetY = 10;
-ctx.shadowBlur = 2;
-ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-
-ctx.beginPath();
-ctx.moveTo(100, 10);
-ctx.lineTo(310, 0);
-ctx.lineTo(520, 10);
-ctx.lineTo(530, 145);
-ctx.lineTo(520, 280);
-ctx.lineTo(310, 290);
-ctx.lineTo(100, 280);
-ctx.lineTo(90, 145);
-ctx.lineTo(100, 10);
-ctx.fillStyle = 'white';
-ctx.fill();
-
-ctx.shadowColor = 'white';
-ctx.font = '16px PT Mono';
-ctx.fillStyle = 'black';
-ctx.fillText('Ееее победа !', 110, 30);
-ctx.fillText('Список результатов:', 110, 50);
-
-var max = -1;
-for (var i = 0; i < times.length; i++) {
-  if (times[i] > max) {
-    max = times[i];
-  }
-}
-
-var gistoHeight = [];
-  for (var j = 0; j < times.length; j++) {
-    gistoHeight[j] = (150*times[j])/max;
-    ctx.fillText(times[j], 150 + 90*j, 220 - gistoHeight[j]);
-    ctx.fillText(names[j], 150 + 90*j, 260);
-
-}
+'use strict';
 
 function randomInteger(min, max) {
   var rand = min - 0.5 + Math.random()*(max - min + 1);
@@ -51,6 +11,60 @@ function getRandomColor() {
   var g = randomInteger(0, 255);
   var b = randomInteger(0, 255);
   return 'rgba('+r+', '+g+', '+b+', 1)';
+}
+
+function setShadowParameters(ctx, newParameters) {
+	ctx.shadowOffsetX = newParameters.offsetX;
+	ctx.shadowOffsetY = newParameters.offsetY;
+	ctx.shadowBlur = newParameters.shadowBlur;
+}
+
+function renderStatistics(ctx, names, times) {
+
+  var oldShadowParameters = {offsetX : ctx.shadowOffsetX,
+  					   	             offsetY : ctx.shadowOffsetY,
+  						               shadowBlur : ctx.shadowBlur};
+
+  setShadowParameters(ctx, {offsetX : 10,
+  						              offsetY : 10,
+  						              shadowBlur : 2});
+
+  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+
+  ctx.beginPath();
+  ctx.moveTo(100, 10);
+  ctx.lineTo(310, 0);
+  ctx.lineTo(520, 10);
+  ctx.lineTo(530, 145);
+  ctx.lineTo(520, 280);
+  ctx.lineTo(310, 290);
+  ctx.lineTo(100, 280);
+  ctx.lineTo(90, 145);
+  ctx.lineTo(100, 10);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+
+  setShadowParameters(ctx, oldShadowParameters);
+
+  ctx.shadowColor = 'white';
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = 'black';
+  ctx.fillText('Ееее победа !', 140, 30);
+  ctx.fillText('Список результатов:', 110, 50);
+
+var max = -1;
+for (var i = 0; i < times.length; i++) {
+  if (times[i] > max) {
+    max = times[i];
+  }
+}
+
+var gistoHeight = [];
+  for (var j = 0; j < times.length; j++) {
+    gistoHeight[j] = (150*times[j])/max;
+    ctx.fillText(parseInt(times[j]), 150 + 90*j, 220 - gistoHeight[j]);
+    ctx.fillText(names[j], 150 + 90*j, 260);
+
 }
 
 for (var j = 0; j < times.length; j++) {
