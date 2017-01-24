4895add60803cab851932bd628b7d88bdfa0d89c
IcyCrumb <ludysh21@gmail.com>
2017-01-24 18:11:44 +0300

    Дз2 финал

--- js/stat.js
+++ js/stat.js
@@ -29,16 +29,28 @@ function setShadowParameters(ctx, newParameters) {
 	ctx.shadowBlur = newParameters.shadowBlur;
 }
 
-//var gistoHeight = [];
+function drawCloud(ctx) {
+  ctx.beginPath();
+  ctx.moveTo(100, 10);
+  ctx.lineTo(310, 0);
+  ctx.lineTo(520, 10);
+  ctx.lineTo(530, 145);
+  ctx.lineTo(520, 280);
+  ctx.lineTo(310, 290);
+  ctx.lineTo(100, 280);
+  ctx.lineTo(90, 145);
+  ctx.lineTo(100, 10);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+}
+
 function fillTextInformation(ctx, times, names, gistoHeight) {
   ctx.font = '16px PT Mono';
   ctx.fillStyle = 'black';
-  ctx.fillText('Ееее победа !', 140, 30);
-  ctx.fillText('Список результатов:', 110, 50);
+  ctx.fillText('Ееее победа !', 150, 30);
+  ctx.fillText('Список результатов:', 150, 50);
 
-//  var gistoHeight = [];
   for (var j = 0; j < times.length; j++) {
-      //gistoHeight[j] = (150*times[j])/getMax(times);
       ctx.fillText(parseInt(times[j]), 150 + 90*j, 220 - gistoHeight[j]);
       ctx.fillText(names[j], 150 + 90*j, 260);
   }
@@ -58,7 +70,6 @@ function drawGistogram(ctx, times, names, gistoHeight) {
 
 
 function renderStatistics(ctx, names, times) {
-
   var oldShadowParameters = {offsetX : ctx.shadowOffsetX,
   					   	             offsetY : ctx.shadowOffsetY,
   						               shadowBlur : ctx.shadowBlur};
@@ -69,72 +80,13 @@ function renderStatistics(ctx, names, times) {
 
   ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
 
-  ctx.beginPath();
-  ctx.moveTo(100, 10);
-  ctx.lineTo(310, 0);
-  ctx.lineTo(520, 10);
-  ctx.lineTo(530, 145);
-  ctx.lineTo(520, 280);
-  ctx.lineTo(310, 290);
-  ctx.lineTo(100, 280);
-  ctx.lineTo(90, 145);
-  ctx.lineTo(100, 10);
-  ctx.fillStyle = 'white';
-  ctx.fill();
-
+  drawCloud(ctx);
   setShadowParameters(ctx, oldShadowParameters);
 
-/*
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = 'black';
-  ctx.fillText('Ееее победа !', 140, 30);
-  ctx.fillText('Список результатов:', 110, 50);
-*/
-
-/*var gistoHeight = [];
-  for (var j = 0; j < times.length; j++) {
-    gistoHeight[j] = (150*times[j])/getMax(times);
+  var gistoHeight = [];
+    for (var j = 0; j < times.length; j++) {
+      gistoHeight[j] = (150*times[j])/getMax(times);
+    }
+  fillTextInformation(ctx, times, names, gistoHeight);
+  drawGistogram(ctx, times, names, gistoHeight);
 }
-*/
-var gistoHeight = [];
-  for (var j = 0; j < times.length; j++) {
-    gistoHeight[j] = (150*times[j])/getMax(times);
-}
-fillTextInformation(ctx, times, names, gistoHeight);
-drawGistogram(ctx, times, names, gistoHeight);
-}
-/*
-var max = -1;
-for (var i = 0; i < times.length; i++) {
-  if (times[i] > max) {
-    max = times[i];
-  }
-}
-*/
-
-/*
-var gistoHeight = [];
-  for (var j = 0; j < times.length; j++) {
-    gistoHeight[j] = (150*times[j])/getMax(times);
-}
-*/
-
-    /*
-    ctx.fillText(parseInt(times[j]), 150 + 90*j, 220 - gistoHeight[j]);
-    ctx.fillText(names[j], 150 + 90*j, 260);
-
-}
-*/
-
-/*
-for (var j = 0; j < times.length; j++) {
-  if (names[j] == 'Вы') {
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  }
-  else {
-    ctx.fillStyle = getRandomColor();
-  }
-  ctx.fillRect(150 + 90*j , 230 - gistoHeight[j], 40, gistoHeight[j]);
-  }
-}
-*/
