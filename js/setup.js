d08d1374cb2ac905484b58c88c813b604dacea5e
IcyCrumb <ludysh21@gmail.com>
2017-01-26 22:01:29 +0300

    Открытие/закрытие окна настройки персонажа

--- js/setup.js
+++ js/setup.js
@@ -1,1 +1,13 @@
+'use strict';
 
+var seekAvatar = document.querySelector('.setup-open');
+
+seekAvatar.addEventListener('click', function() {
+  document.querySelector('div.overlay.setup').classList.remove('invisible');
+});
+
+var seekCross = document.querySelector('.setup-close');
+
+seekCross.addEventListener('click', function() {
+  document.querySelector('div.overlay.setup').classList.add('invisible');
+});
