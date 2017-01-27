5be0e48fde73f6b7d3f1488c1ef362daa6f4f82f
IcyCrumb <ludysh21@gmail.com>
2017-01-27 14:40:00 +0300

    Выбор цвета мантии

--- js/setup.js
+++ js/setup.js
@@ -1,17 +1,24 @@
 'use strict';
 
-var seekAvatar = document.querySelector('.setup-open');
+var seekAvatar = document.querySelector(".setup-open");
 
 seekAvatar.addEventListener('click', function() {
-  document.querySelector('div.overlay.setup').classList.remove('invisible');
+  document.querySelector("div.overlay.setup").classList.remove("invisible");
 });
 
-var seekCross = document.querySelector('.setup-close');
+var seekCross = document.querySelector(".setup-close");
 
-seekCross.addEventListener('click', function() {
-  document.querySelector('div.overlay.setup').classList.add('invisible');
+seekCross.addEventListener("click", function() {
+  document.querySelector("div.overlay.setup").classList.add("invisible");
 });
 
-var nameField = document.querySelector('.setup-user-name');
+var nameField = document.querySelector(".setup-user-name");
 nameField.required = true;
 nameField.maxLength = 50;
+
+var smth = document.getElementById("wizard-coat");
+smth.addEventListener("click", function() {
+  var coatColor = ["rgb(101, 137, 164)", "rgb(241, 43, 107)", "rgb(146, 100, 161)", "rgb(56, 159, 117)", "rgb(215, 210, 55)", "rgb(0, 0, 0)"];
+  var rand = Math.floor(Math.random()*coatColor.length);
+    smth.style.fill = coatColor[rand];
+});
