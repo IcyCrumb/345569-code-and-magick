83ba2c2ef7a6d7cc15ee92ce3aef61b7e275cfac
IcyCrumb <ludysh21@gmail.com>
2017-01-27 18:32:04 +0300

    Проверка реакции

--- js/setup.js
+++ js/setup.js
@@ -6,7 +6,7 @@ seekAvatar.addEventListener('click', function() {
   document.querySelector("div.overlay.setup").classList.remove("invisible");
 });
 
-var seekCross = document.querySelector(".setup-close");
+
 
 seekCross.addEventListener("click", function() {
   document.querySelector("div.overlay.setup").classList.add("invisible");
