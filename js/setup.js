ba4cabff69265e73753285cc728612557619a229
IcyCrumb <ludysh21@gmail.com>
2017-01-27 16:13:17 +0300

    Цвет глаз мага

--- js/setup.js
+++ js/setup.js
@@ -16,9 +16,16 @@ var nameField = document.querySelector(".setup-user-name");
 nameField.required = true;
 nameField.maxLength = 50;
 
-var smth = document.getElementById("wizard-coat");
-smth.addEventListener("click", function() {
+var seekWizardCoat = document.getElementById("wizard-coat");
+seekWizardCoat.addEventListener("click", function() {
   var coatColor = ["rgb(101, 137, 164)", "rgb(241, 43, 107)", "rgb(146, 100, 161)", "rgb(56, 159, 117)", "rgb(215, 210, 55)", "rgb(0, 0, 0)"];
   var rand = Math.floor(Math.random()*coatColor.length);
-    smth.style.fill = coatColor[rand];
+  seekWizardCoat.style.fill = coatColor[rand];
+});
+
+var seekWizardEyes = document.getElementById("wizard-eyes");
+seekWizardEyes.addEventListener("click", function() {
+  var eyesColor = ["black", "red", "blue", "yellow", "green"];
+  var rand = Math.floor(Math.random()*eyesColor.length);
+  seekWizardEyes.style.fill = eyesColor[rand];
 });
